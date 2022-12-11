09ee3de510ee930c92357bd103508a191a52c047
nguyenquocchauit <chauquocnguyen.cun1@gmail.com>
2022-12-11 19:41:16 +0700

    handle 50% function login user

--- Watch/webpack.mix.js
+++
@@ -1,7 +0,0 @@
-mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .copy(
-        'node_modules/@fortawesome/fontawesome-free/webfonts',
-        'public/webfonts'
-    );
